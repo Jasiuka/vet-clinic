92cd9de050e7c2114619d5bdac3038f62b581ea0
Jasiuka <luksjasiukaitis@gmail.com>
2023-10-10 22:02:06 +0300

    some fix

--- src/pages/appointment-registration.page.jsx
+++ src/pages/appointment-registration.page.jsx
@@ -1,8 +1,8 @@
-import { useGetAppointmentsByDateRangeQuery } from "../services/appointments";
-import { appointmentsApi } from "../services/appointments";
+import { useParams } from "react-router-dom";
 export const AppointmentRegistration = () => {
-  const result = appointmentsApi.endpoints.getAppointmentsByDateRange;
-  console.log(result);
+  let { id } = useParams();
+  // const result = appointmentsApi.endpoints.getAppointmentsByDateRange;
+  console.log(id);
   return <h1 className="for-observer">Hello world</h1>;
 };
 
